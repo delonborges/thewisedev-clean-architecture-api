03dbe31bbadf70d0c0043407fe6f34f614883859
Delon <delon_borges@sicredi.com.br>
2023-08-23 10:23:06 -0300

    feat: validate domain part larger than 255 chars

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -7,9 +7,7 @@ export class Email {
             return false
         }
         const [local, domain] = email.split('@')
-        if (local.length > 64) {
-            return false
-        }
-        return true
+        return !(local.length > 64 || domain.length > 255);
+
     }
 }
