19c5324931865f9f48e6868881a5cf32ce3dc126
Delon <delon_borges@sicredi.com.br>
2023-08-23 10:23:28 -0300

    test: validate domain part larger than 255 chars

--- src/entities/email-validation.spec.ts
+++ src/entities/email-validation.spec.ts
@@ -18,6 +18,10 @@ describe('Email validation', () => {
         const email = 'l'.repeat(65) + '@mail.com'
         expect(Email.validate(email)).toBeFalsy()
     })
+    test('should not accept domain part larger than 255 chars', () => {
+        const email = 'local@' + 'd'.repeat(252) + '.com'
+        expect(Email.validate(email)).toBeFalsy()
+    })
     test('should not accept strings larger than 320 chars', () => {
         const email = 'l'.repeat(64) + '@' + 'd'.repeat(252) + '.com'
         expect(Email.validate(email)).toBeFalsy()
