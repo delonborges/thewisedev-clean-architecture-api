b559a5b6f6795b698572f5c510479a983b762cac
Delon Borges <delon.borges@icloud.com>
2023-08-23 21:30:43 -0300

    feat: validate emails with empty local part

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -1,13 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/no-extraneous-class
 export class Email {
-    static validate(email: string): boolean {
-        if (!email) {
-            return false
-        }
-        if (email.length > 320) {
-            return false
-        }
-        const [local, domain] = email.split('@')
-        return !(local.length > 64 || domain.length > 255);
-
+  static validate (email: any): boolean {
+    if (email == null || email === '') {
+      return false
     }
+    if (email.length > 320) {
+      return false
+    }
+    const [local, domain] = email.split('@')
+    return !(local.length === 0 || local.length > 64 || domain.length > 255)
+  }
 }
